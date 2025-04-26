f605bd0d4a8b1043abdb626ec9fc07094dae29bb
Sam Caldwell <github@samcaldwell.net>
2025-04-26 18:15:23 -0500

    make build_all.cmake explicit

--- cmake/build_all.cmake
+++ cmake/build_all.cmake
@@ -1,15 +1,18 @@
 # build_all.cmake
 # Build all enabled projects (see feature flags defined in CMakeLists.txt
 include(build_compress)
+include(build_decompress)
 # ToDo: add more command artifacts here
 include(build_test_compress_flag_usage)
 include(build_test_compress_flag_version)
 # ToDo: add more test artifacts here
 
 add_custom_target(build_all
-    COMMENT("build all the projects")
-    DEPENDS test_compress_flag_usage
-            test_compress_flag_version
-            compress
-    VERBATIM
+        COMMENT "Build all the projects"
+        ${CMAKE_COMMAND} --build build --target compress
+        ${CMAKE_COMMAND} --build build --target decompress
+        ${CMAKE_COMMAND} --build build --target test_compress_flag_usage
+        ${CMAKE_COMMAND} --build build --target test_compress_flag_version
+        DEPENDS compress decompress test_compress_flag_usage test_compress_flag_version
+        VERBATIM
 )
