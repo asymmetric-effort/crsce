2072b155b18ead499b0c8f981387d6a4950cf67d
Sam Caldwell <github@samcaldwell.net>
2025-04-27 00:51:38 -0500

    CrossSum class

--- include/CRSCE/CrossSum/CrossSum.h
+++ include/CRSCE/CrossSum/CrossSum.h
@@ -10,36 +10,16 @@
 
 class CrossSum {
 public:
-    explicit CrossSum(const unsigned int size): s(size);
+    explicit CrossSum(const CrossSumValue size,const CrossSumValue width): s(size);
     ~CrossSum();
 
-    virtual unsigned int get(unsigned int r, unsigned int c) const;
-    virtual void set(unsigned int r, unsigned int c, unsigned int value);
-    virtual void increment(unsigned int r, unsigned int c);
+    virtual CrossSumValue get(CrossSumValue r, CrossSumValue c) const;
+    virtual void set(CrossSumValue r, CrossSumValue c, CrossSumValue value);
+    virtual void increment(CrossSumValue r, CrossSumValue c);
 
 private:
-    const unsigned int s;
-    std::vector<unsigned int> data;
+    const CrossSumValue s;
+    std::vector<CrossSumValue> data;
 };
 
 #endif // CRSCE_CROSSSUM_H
-
-
-// file: src/CRSCE/CrossSum.cpp
-// (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
-
-#include "CRSCE/CrossSum.h"
-
-CrossSum::CrossSum(unsigned int size) : s(size), data(size, 0) {}
-
-unsigned int CrossSum::get(unsigned int r, unsigned int c) const {
-    return data[xsm(r, c, s)];
-}
-
-void CrossSum::set(unsigned int r, unsigned int c, unsigned int value) {
-    data[xsm(r, c, s)] = value;
-}
-
-void CrossSum::increment(unsigned int r, unsigned int c) {
-    ++data[xsm(r, c, s)];
-}
