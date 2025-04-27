55d3900fb2d69275b07393280397c69a65b8d2a0
Sam Caldwell <github@samcaldwell.net>
2025-04-26 22:37:58 -0500

    update ByteBuffer

--- include/CRSCE/ByteBuffer.h
+++ include/CRSCE/ByteBuffer.h
@@ -1,11 +1,11 @@
 // file: include/CRSCE/ByteBuffer.h
 // (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
 
-#ifndef CRSCE_BYTES_BUFFER_H
-#define CRSCE_BYTES_BUFFER_H
+#ifndef CRSCE_BYTEBUFFER_H
+#define CRSCE_BYTEBUFFER_H
 
 #include <vector>
 
 using ByteBuffer = std::vector<uint8_t>;
 
-#endif //CRSCE_BYTES_BUFFER_H
+#endif //CRSCE_BYTEBUFFER_H
