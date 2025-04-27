b9b2e760a6f53d63f05fb9f7ed9d84fcf0e6a6f1
Sam Caldwell <github@samcaldwell.net>
2025-04-26 22:28:28 -0500

    add utils/generateRandomFile()

---
+++ include/utils/generateRandomFile.h
@@ -0,0 +1,9 @@
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <vector>
+#include <cstring>
+#include <filesystem>
+
+// Generate a random file of a given size in bytes
+void generateRandomFile(const std::string& filename, const int file_size);
