0839cdacbf3f23496a9fa36436f97090adc6bb6b
Sam Caldwell <github@samcaldwell.net>
2025-04-26 22:27:48 -0500

    add ByteBuffer type

---
+++ include/CRSCE/ByteBuffer.h
@@ -0,0 +1,11 @@
+// file: include/CRSCE/ByteBuffer.h
+// (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
+
+#ifndef CRSCE_BYTES_BUFFER_H
+#define CRSCE_BYTES_BUFFER_H
+
+#include <vector>
+
+using ByteBuffer = std::vector<uint8_t>;
+
+#endif //CRSCE_BYTES_BUFFER_H
