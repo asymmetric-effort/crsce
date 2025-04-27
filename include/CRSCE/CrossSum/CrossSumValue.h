9a138b697cf1fdcac97930a64f5bf0e40adf9709
Sam Caldwell <github@samcaldwell.net>
2025-04-27 00:51:09 -0500

    Add CrossSumValue as std::bitset

--- include/CRSCE/CrossSum/CrossSumValue.h
+++ include/CRSCE/CrossSum/CrossSumValue.h
@@ -1,8 +1,12 @@
 // file: include/CRSCE/CrossSumValue.h
 // (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
+
 #ifndef CRSCE_CROSSSUM_VALUE_H
 #define CRSCE_CROSSSUM_VALUE_H
 
-using CrossSumValue = unsigned int;
+#include <bitset>
 
-#endif //CRSCE_CROSSSUM_VALUE_H
+// Bit field where w = ceil(log2(512)) = 9.
+using CrossSumValue = std::bitset<9>;
+
+#endif // CRSCE_CROSSSUM_VALUE_H
