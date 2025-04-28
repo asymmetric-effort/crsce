1bcd14a9c00f537671bbb37174ac6b8c7ad959fc
Sam Caldwell <github@samcaldwell.net>
2025-04-27 19:20:23 -0500

    debugging and cleanup

--- src/CRSCE/CrossSum/DiagonalSumMatrix/increment.cpp
+++ src/CRSCE/CrossSum/DiagonalSumMatrix/increment.cpp
@@ -1,8 +1,8 @@
-// file: include/CRSCE/CrossSum/DiagonalSumsMatrix/increment.cpp
+// file: include/CRSCE/CrossSum/DiagonalSumMatrix/increment.cpp
 // (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
 
 #include "CRSCE/CrossSum/DiagonalSumMatrix/DiagonalSumMatrix.h"
 
-void DiagonalSumsMatrix::increment(const CrossSumIndex r, const CrossSumIndex c) {
+void DiagonalSumMatrix::increment(const CrossSumIndex r, const CrossSumIndex c) {
     data[transform(r,c)]++;
 }
