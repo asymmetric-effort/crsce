e4434d6c164106f50a2175e2dfd09c3ba75d795b
Sam Caldwell <github@samcaldwell.net>
2025-04-29 10:51:13 -0500

    fix test/CrossSumValue overflow error

--- test/CrossSumValue.cpp
+++ test/CrossSumValue.cpp
@@ -1,18 +1,18 @@
 // file: test/CrossSumValue.cpp
 // (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
 
+#include "CRSCE/constants/constants.h"
 #include "CRSCE/CrossSum/CrossSumValue/CrossSumValue.h"
 #include <iostream>
 #include <cstdlib>
 #include <cstdint>
 #include <stdexcept>
 
-constexpr uint16_t max_value=(1 << 9)-1; // all bits set
-
 /**
  * confirm that the default constructor initializes a zero-state.
  */
 int test_default_constructor() {
+    std::cout << "test_default_constructor()" << std::endl;
     CrossSumValue value;
     // Confirm initial state is 0
     if (value.to_uint16() != 0) {
@@ -24,7 +24,8 @@ int test_default_constructor() {
 
 // confirm we can set an initial value from 0 to 2^b-1
 int test_initial_value(){
-    for(uint16_t initial_value=0; initial_value < max_value; initial_value++){
+    std::cout << "test_initial_value()" << std::endl;
+    for(uint16_t initial_value=0; initial_value < s; initial_value++){
         CrossSumValue value(initial_value);
         if (value.to_uint16() != initial_value) {
             std::cerr << "[FAIL] Initial value (" << std::to_string(value.to_uint16()) << ") "
@@ -39,17 +40,12 @@ int test_initial_value(){
 // Test increment and expect overflow
 int test_increment(){
      std::cout << "test_increment() [step 1]" << std::endl;
-    // increment through the available numbers
-    constexpr uint16_t max_value=(1 << 9)-1; // all bits set
     // setup the test
     CrossSumValue value(0);
-    for(uint16_t expected_value=0;expected_value<max_value;expected_value++){
+    for(uint16_t expected_value=0;expected_value<s;expected_value++){
         if (uint16_t actual_value=value.to_uint16();actual_value != expected_value){
-            std::cerr << "[FAIL] expected_value("
-                      << std::to_string(expected_value)
-                      << ") missed. got "
-                      << std::to_string(actual_value)
-                      << std::endl;
+            std::cerr << "[FAIL] expected_value(" << std::to_string(expected_value) << ") missed. got "
+                      << std::to_string(actual_value) << std::endl;
             return EXIT_FAILURE;
         }
         value++;
@@ -59,7 +55,7 @@ int test_increment(){
         value++;
         std::cerr << "[FAIL] overflow_error expected(1).  not encountered.\n\t"
                      "got:  " << std::to_string(value.to_uint16()) << "\n"
-                     "want: " << std::to_string(max_value) << std::endl;
+                     "want: " << std::to_string(s) << std::endl;
         return EXIT_FAILURE;
     }catch(std::overflow_error const &e){
       return EXIT_SUCCESS;
@@ -70,12 +66,12 @@ int test_add_operator(){
     std::cout << "test_add_operator() [step 1]" << std::endl;
     {
         CrossSumValue value1(1);
-        CrossSumValue value2(max_value);
+        CrossSumValue value2(s);
         try {
             CrossSumValue value3(value1 + value2);
             std::cerr << "[FAIL] overflow_error expected(2).  not encountered.\n\t"
                          "got:  " << std::to_string(value3.to_uint16()) << "\n"
-                         "want: " << std::to_string(max_value) << std::endl;
+                         "want: " << std::to_string(s) << std::endl;
             return EXIT_FAILURE;
         } catch (const std::overflow_error& e) {
             // expected overflow
