36360e44cff5b7cd59553336b8c6aecfef9cbab1
Sam Caldwell <github@samcaldwell.net>
2025-04-28 22:39:57 -0500

    code cleanup

--- test/verify-lateral-sum-matrix.cpp
+++ test/verify-lateral-sum-matrix.cpp
@@ -5,19 +5,15 @@
 #include <iostream>
 #include <cstdlib>
 
+constexpr size_t block_size = 512;
+constexpr size_t cross_sum_width = 9;
+
 int verify_100pct_set(){
-
-    constexpr size_t block_size = 512;
-
-    constexpr size_t cross_sum_width = 9;
-
     LateralSumMatrix lsm(block_size, cross_sum_width);
-
     std::cout << "verify_100pct_set()" << std::endl;
     for (uint16_t r = 0; r < block_size; ++r)
         for (uint16_t c = 0; c < block_size - 1; ++c) // 0..510
             lsm.increment(r, c);
-
     for(uint16_t r = 0; r < block_size; r++)
         if(auto sum=lsm.get(r,r).to_uint16(); sum != (block_size-1)){
             std::cerr << "[FAIL] Verification(1/3): LSM value expects full set value." << std::endl;
@@ -27,13 +23,10 @@ int verify_100pct_set(){
 }
 
 int verify_1_bit_per_row_set() {
-    constexpr size_t block_size = 512;
-    constexpr size_t cross_sum_width = 9;
     LateralSumMatrix lsm(block_size, cross_sum_width);
     std::cout << "verify_1_bit_per_row_set()" << std::endl;
     for (uint16_t r = 0; r < block_size; ++r)
       lsm.increment(r, r); // set diagonal pattern
-
     for (uint16_t r = 0; r < block_size; ++r)
         if (auto sum = lsm.get(r, r).to_uint16(); sum != 1) {
             std::cerr << "[FAIL] Verification(1/3): LSM value expects 1 value." << std::endl;
@@ -43,8 +36,6 @@ int verify_1_bit_per_row_set() {
 }
 
 int verify_overlow_works(){
-    constexpr size_t block_size = 512;
-    constexpr size_t cross_sum_width = 9;
     LateralSumMatrix lsm(block_size, cross_sum_width);
     std::cout << "verify_overlow_works()" << std::endl;
     try{
@@ -60,23 +51,14 @@ int verify_overlow_works(){
 
 int main() {
     try {
-
         if(int exit=verify_100pct_set();exit!=EXIT_SUCCESS) return EXIT_FAILURE;
-
         if(int exit=verify_1_bit_per_row_set();exit!=EXIT_SUCCESS) return EXIT_FAILURE;
-
         if(int exit=verify_overlow_works();exit!=EXIT_SUCCESS) return EXIT_FAILURE;
 
         std::cout << "[PASS] LateralSumMatrix row-major encoding verified." << std::endl;
-
         return EXIT_SUCCESS;
-
     } catch (const std::exception& e) {
-
         std::cerr << "[ERROR] Exception: " << e.what() << std::endl;
-
         return EXIT_FAILURE;
-
     }
-
 }
