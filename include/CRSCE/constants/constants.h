4fc70a18f1e3625a4cf24259a009026d8309b32f
Sam Caldwell <github@samcaldwell.net>
2025-04-29 09:36:56 -0500

    add a single constants file for s and b

---
+++ include/CRSCE/constants/constants.h
@@ -0,0 +1,12 @@
+// file: include/CRSCE/constants/constants.h
+// (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
+#ifndef CSRCE_CONSTANTS_H
+#define CSRCE_CONSTANTS_H
+
+//Represents the CRSCE s-value.
+constexpr size_t s = 512-1
+
+//Represents the CRSCE b-value.  b=ciel(log2(s))
+static constexpr size_t b = 9;
+
+#endif //CSRCE_CONSTANTS_H
