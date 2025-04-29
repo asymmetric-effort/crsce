4c7dbbd47313b778c83832e5f766cd29480f03e7
Sam Caldwell <github@samcaldwell.net>
2025-04-29 09:52:24 -0500

    add max_index constant

--- include/CRSCE/constants/constants.h
+++ include/CRSCE/constants/constants.h
@@ -4,7 +4,11 @@
 #define CSRCE_CONSTANTS_H
 
 //Represents the CRSCE s-value.
-constexpr size_t s = 512-1
+//  512-1 = 511 is the maximum number of rows and columns allowed when b=9
+//  This means our coordinates should always be 0...510 x 0...510.
+//  our CSM uncompressed size is 511^2.
+constexpr size_t s = 512-1; //511
+constexpr size_t max_index = s-1; //510
 
 //Represents the CRSCE b-value.  b=ciel(log2(s))
 static constexpr size_t b = 9;
