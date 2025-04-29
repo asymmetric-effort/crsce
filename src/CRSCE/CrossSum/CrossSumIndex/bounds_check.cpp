83849268a35b50aa58dbc5f1f30f263b06543c1f
Sam Caldwell <github@samcaldwell.net>
2025-04-29 10:17:04 -0500

    fix bounds_check()

--- src/CRSCE/CrossSum/CrossSumIndex/bounds_check.cpp
+++ src/CRSCE/CrossSum/CrossSumIndex/bounds_check.cpp
@@ -3,7 +3,7 @@
 
 #include "CRSCE/CrossSum/CrossSumIndex/CrossSumIndex.h"
 
-void bounds_check(const CrossSumIndex index, const CrossSumIndex max_value){
+void bounds_check(const CrossSumIndex index){
     if (index > max_index) {
         throw std::overflow_error("Index out of bounds");
     }
