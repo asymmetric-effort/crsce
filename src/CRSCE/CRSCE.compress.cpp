f7ef591a84963f8399ebd796e5748ef497f3c139
Sam Caldwell <github@samcaldwell.net>
2025-05-01 09:58:28 -0500

    fix CRSCE::compress() overflow issue.

--- src/CRSCE/CRSCE.compress.cpp
+++ src/CRSCE/CRSCE.compress.cpp
@@ -12,13 +12,12 @@ int CRSCE::compress() {
         std::cerr << "[CRSCE] Compression starting..." << std::endl;
         FileBuffer inputBuffer;
 
-        uint32_t bit_index = 0;
-        uint64_t block_count = 0;
-
         // Write the file header (HEADER_LENGTH bytes)
         outputStream.write(HEADER, HEADER_LENGTH);
 
+        uint64_t block_count = 0;
         for (size_t sz = 0; readInputBuffer(inputBuffer); sz += inputBuffer.size()) {
+
             std::cout << "[CRSCE] Processing " << std::to_string(sz) << " bytes." << std::endl;
 
             LHashMatrix LHASH;
@@ -27,34 +26,49 @@ int CRSCE::compress() {
             DiagonalSumMatrix XSM;
             AntidiagonalSumMatrix DSM;
 
+            uint32_t bit_index = 0;
+            constexpr uint32_t block_limit = s * s; // limit on bit_index to avoid overflow.
             for (const auto& word : inputBuffer) {
                 for (int bit = FILE_BUFFER_WIDTH - 1; bit >= 0; --bit) {
-                    bool bit_value = (word >> bit) & 0x01;
-                    std::cout << "bit index: " << bit_index << " value: " << std::to_string(bit_value) << std::endl;
 
+                    if (bit_index >= block_limit) break;
+
+                    bool bit_value = (word >> bit) & 0x01;
                     CrossSumIndex r = bit_index / s;
                     CrossSumIndex c = bit_index % s;
+                    std::cout << "bit index: " << bit_index << " ("
+                              << std::to_string(r) << ","
+                              << std::to_string(c) << ") "
+                              << "value: " << std::to_string(bit_value)
+                              << std::endl;
 
-                    if (bit_value) {
+                    try{
+                        if (bit_value) {
+                            LSM.increment(r, c);
+                            VSM.increment(r, c);
+                            XSM.increment(r, c);
+                            DSM.increment(r, c);
+                        }
                         LHASH.push(r, c, bit_value);
-                        LSM.increment(r, c);
-                        VSM.increment(r, c);
-                        XSM.increment(r, c);
-                        DSM.increment(r, c);
+                    }catch(std::exception& e){
+                        std::cerr << "cross sum ::increment(r, c) error: " << e.what() << std::endl;
+                        return EXIT_FAILURE;
                     }
-
                     ++bit_index;
                 }
             }
-            // Pad all remaining rows/cells in the block if EOF ends mid-block
-            const CrossSumIndex start_r = bit_index / s;
-            for (CrossSumIndex r = start_r; r < s; ++r) {
-                const CrossSumIndex start_c = (r == start_r ? bit_index % s : 0);
-                for (CrossSumIndex c = start_c; c < s; ++c) {
-                    LHASH.push(r, c, false);
+            if (bit_index % s != 0) {
+                std::cout << "[CRSCE] adding padding bits..." << std::endl;
+                // Pad all remaining rows/cells in the block if EOF ends mid-block
+                const CrossSumIndex start_r = bit_index / s;
+                for (CrossSumIndex r = start_r; r < s; ++r) {
+                    const CrossSumIndex start_c = (r == start_r ? bit_index % s : 0);
+                    for (CrossSumIndex c = start_c; c < s; ++c) {
+                        LHASH.push(r, c, false);
+                    }
                 }
+                std::cout << "[CRSCE] ...padding complete." << std::endl;
             }
-
             LHASH.serialize(outputStream);
             LSM.serialize(outputStream);
             VSM.serialize(outputStream);
