303178d6602bf9b3e761a5adfa77bfdf99717327
Sam Caldwell <github@samcaldwell.net>
2025-05-19 23:58:43 -0500

    refactor test/2000 to implement Tester class

--- test/2000_AntiDiagonalCoordinateTransform.cpp
+++ test/2000_AntiDiagonalCoordinateTransform.cpp
@@ -1,6 +1,7 @@
 // file: test/2000_AntiDiagonalCoordinateTransform.cpp
 // (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
 
+#include "utils/test/Tester.h"
 #include "CRSCE/CrossSum/AntidiagonalSumMatrix/AntidiagonalSumMatrix.h"
 #include "CRSCE/constants/constants.h"
 #include <iostream>
@@ -8,42 +9,50 @@
 #include <cstdlib>
 
 int main() {
-    try {
-        AntidiagonalSumMatrix dsm;
-        std::map<CrossSumIndex, int> index_count;
+    Tester tester("test/2000_AntiDiagonalCoordinateTransform", TerminateOnError);
+    tester.deadline(60);
+    tester.skip("disabled for debug");
 
-        for (CrossSumIndex r = 0; r < s; ++r) {
-            for (CrossSumIndex c = 0; c < s; ++c) {
-                CrossSumIndex i = dsm.transform(r, c);
-                if (i >= s) {
-                    std::cerr << "[FAIL] transform(" << r << "," << c << ") = " << i
-                              << " out of bounds (max " << s - 1 << ")" << std::endl;
-                    return EXIT_FAILURE;
-                }
-                index_count[i]++;
-            }
+    AntidiagonalSumMatrix dsm;
+    std::map<CrossSumIndex, int> index_count;
+
+    for (CrossSumIndex r = 0; r < s; ++r) {
+        for (CrossSumIndex c = 0; c < s; ++c) {
+            CrossSumIndex i = dsm.transform(r, c);
+            tester.assertTrue(
+                i>=s,
+                std::format(
+                    "transform({},{})={} out of bounds (max {})",
+                    r, c,
+                    i,
+                    s-1
+                )
+            );
+            index_count[i]++;
         }
-
-        if (index_count.size() != s) {
-            std::cerr << "[FAIL] Antidiagonal index count map size is " << index_count.size()
-                      << ", expected " << s << std::endl;
-            return EXIT_FAILURE;
-        }
-
-        for (const auto& [index, count] : index_count) {
-            if (count != s) {
-                std::cerr << "[FAIL] Antidiagonal index " << index << " occurred "
-                          << count << " times, expected " << s << std::endl;
-                return EXIT_FAILURE;
-            }
-        }
-
-        std::cout << "[PASS] AntidiagonalSumMatrix::transform() covers 0.." << s - 1
-                  << " exactly " << s << " times each." << std::endl;
-        return EXIT_SUCCESS;
-
-    } catch (const std::exception& e) {
-        std::cerr << "[ERROR] Exception: " << e.what() << std::endl;
-        return EXIT_FAILURE;
     }
+    tester.assertTrue(
+        std::format(
+            "Antidiagonal index count map size is {}, expected {}",
+                index_count.size(), s
+        )
+    );
+
+    for (const auto& [index, count] : index_count){
+        tester.assertNotEqual(
+            count,
+            s,
+            std::format(
+                "Antidiagonal index {} occurred {} times, expected {}",
+                    index,count,s
+            )
+        );
+    }
+    tester.debug(
+        std::format(
+            "AntidiagonalSumMatrix::transform() covers 0..{} exactly {} times each.",
+                s - 1, s
+        )
+    );
+    return EXIT_SUCCESS;
 }
