efcd082e69e921207ab24e1b4a36594c2ab350eb
Sam Caldwell <github@samcaldwell.net>
2025-05-20 00:06:44 -0500

    refactor test/2000* to apply Tester class

--- test/2000_DiagonalCoordinateTransform.cpp
+++ test/2000_DiagonalCoordinateTransform.cpp
@@ -1,6 +1,7 @@
 // file: test/2000_DiagonalCoordinateTransform.cpp
 // (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
 
+#include "utils/test/Tester.h"
 #include "CRSCE/CrossSum/DiagonalSumMatrix/DiagonalSumMatrix.h"
 #include "CRSCE/constants/constants.h"
 #include <iostream>
@@ -8,42 +9,42 @@
 #include <cstdlib>
 
 int main() {
-        try {
-                DiagonalSumMatrix xsm;
-                std::map<CrossSumIndex, int> index_count;
+        Tester tester("test/2000_AntiDiagonalCoordinateTransform", TerminateOnError);
+        tester.deadline(60);
+        tester.skip("disabled for debug");
 
-                for (CrossSumIndex r = 0; r < s; ++r) {
-                        for (CrossSumIndex c = 0; c < s; ++c) {
-                                CrossSumIndex i = xsm.transform(r, c);
-                                if (i >= s) {
-                                        std::cerr << "[FAIL] transform(" << r << "," << c << ") = " << i
-                                                  << " out of bounds (max " << s - 1 << ")" << std::endl;
-                                        return EXIT_FAILURE;
-                                }
-                                index_count[i]++;
-                        }
+        DiagonalSumMatrix xsm;
+        std::map<CrossSumIndex, int> index_count;
+
+        for (CrossSumIndex r = 0; r < s; ++r) {
+                for (CrossSumIndex c = 0; c < s; ++c) {
+                        CrossSumIndex i = xsm.transform(r, c);
+                        tester.AssertTrue(
+                                i >= s,
+                                std::format("transform({},{})={} out of bounds (max {})",r c, i, s - 1)
+                        );
+                        index_count[i]++;
                 }
+        }
 
-                if (index_count.size() != s) {
-                        std::cerr << "[FAIL] Diagonal index count map size is " << index_count.size()
-                                  << ", expected " << s << std::endl;
-                        return EXIT_FAILURE;
-                }
+        tester.AssertNotEqual(
+                index_count.size(),
+                s,
+                std::format(
+                        "Diagonal index count map size is {} expected {}",
+                        index_count.size(),
+                        s
+                )
+        );
 
-                for (const auto& [index, count] : index_count) {
-                        if (count != s) {
-                                std::cerr << "[FAIL] Diagonal index " << index << " occurred "
-                                          << count << " times, expected " << s << std::endl;
-                                return EXIT_FAILURE;
-                        }
-                }
-
-                std::cout << "[PASS] DiagonalSumMatrix::transform() covers 0.." << s - 1
-                          << " exactly " << s << " times each." << std::endl;
-                return EXIT_SUCCESS;
-
-        } catch (const std::exception& e) {
-                std::cerr << "[ERROR] Exception: " << e.what() << std::endl;
-                return EXIT_FAILURE;
+        for (const auto& [index, count] : index_count) {
+                tester.AssertNotEqual(
+                        count,
+                        s,
+                        std::format(
+                                "Diagonal index {} occurred {} times, expected {}",
+                                index, count, s
+                        )
+                );
         }
 }
