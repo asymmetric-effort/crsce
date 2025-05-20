9feaef15defb20fc37ea4cd900ff432b69dbb604
Sam Caldwell <github@samcaldwell.net>
2025-05-19 21:39:33 -0500

    add Gpu::PointerTracker

---
+++ include/Gpu/common/PointerTracker.h
@@ -0,0 +1,10 @@
+// file: src/Gpu/Device/PointerTracker.h
+// (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
+
+#pragma once
+
+#include <unordered_set>
+
+namespace Gpu {
+    using PointerTracker = Gpu::PointerTracker;
+}
