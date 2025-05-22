ec5c441e9dcf0a92ebb77bd6aed9989cb26efa54
Sam Caldwell <github@samcaldwell.net>
2025-05-21 19:42:11 -0500

    CRSCE-2: add improved comments to Emulator class

--- include/Gpu/Device/Emulator/Emulator.h
+++ include/Gpu/Device/Emulator/Emulator.h
@@ -24,21 +24,33 @@ namespace Gpu::Device {
      */
     class Emulator {
     public:
+        /**
+         * @name constructor
+         * @brief default constructor
+         */
         Emulator();
+
+        /**
+         * @name destructor
+         * @brief The class destructor will ensure the class instance is properly shutdown.
+         */
         ~Emulator();
 
         /**
+         * @name init
          * @brief Initialize the emulator. Must be called before other methods.
          * This will fork the child process and establish IPC via Communications.
          */
         bool init();
 
         /**
+         * @name shutdown
          * @brief Gracefully shut down the emulator and clean up resources.
          */
         void shutdown();
 
         /**
+         * @name alloc
          * @brief Allocate a buffer on the emulated GPU.
          * @param bytes The number of bytes to allocate.
          * @return A valid device pointer on success, nullptr on failure.
@@ -46,12 +58,14 @@ namespace Gpu::Device {
         void* alloc(std::size_t bytes);
 
         /**
+         * @name free
          * @brief Free a previously allocated buffer.
          * @param ptr The device pointer to free.
          */
         void free(void* ptr);
 
         /**
+         * @name write
          * @brief Write data to a device buffer.
          * @param ptr Device pointer.
          * @param src Source buffer.
@@ -61,6 +75,7 @@ namespace Gpu::Device {
         bool write(void* ptr, const void* src, std::size_t size);
 
         /**
+         * @name read
          * @brief Read data from a device buffer.
          * @param dst Destination buffer.
          * @param ptr Device pointer.
@@ -70,6 +85,7 @@ namespace Gpu::Device {
         bool read(void *dst, void *ptr, std::size_t size) const;
 
         /**
+         * @name registerKernel
          * @brief Register a kernel for future launch.
          * @param kernelId ID to assign to the kernel.
          * @param binary Kernel binary blob.
@@ -77,6 +93,7 @@ namespace Gpu::Device {
         void registerKernel(uint32_t kernelId, const std::vector<uint8_t>& binary);
 
         /**
+         * @name launchTask
          * @brief Launch a previously registered kernel task.
          * @param kernelId The kernel to launch.
          * @param args Optional argument payload.
@@ -85,14 +102,35 @@ namespace Gpu::Device {
         bool launchTask(uint32_t kernelId, const std::vector<uint8_t>& args = {});
 
         /**
+         * @name reset
          * @brief Reset the device, clearing all allocations.
          */
         void reset();
 
     private:
+        /**
+         * @name childPid_
+         * @brief child process id.
+         * @type pid_t
+         */
         pid_t childPid_ = -1;
+        /**
+         * @name com_
+         * @brief IPC Communications channel manager reference
+         * @type std::unique_ptr<Gpu::Ipc::Communications>
+         */
         std::unique_ptr<Ipc::Communications> comm_;
+        /**
+         * @name tracker_
+         * @brief memory allocation tracker
+         * @type PointerTracker
+         */
         PointerTracker tracker_;
+        /**
+         * @name initialized_
+         * @brief indicates whether Gpu::Emulator is initialized.
+         * @type bool
+         */
         bool initialized_ = false;
     };
 
