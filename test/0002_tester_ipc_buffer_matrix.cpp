e83e03275dabd23edcbbcd51ee1eff4bfc002334
Sam Caldwell <github@samcaldwell.net>
2025-05-26 00:17:22 -0500

    passing test/0002 and test/0003

--- test/0002_tester_ipc_buffer_matrix.cpp
+++ test/0002_tester_ipc_buffer_matrix.cpp
@@ -17,45 +17,65 @@ int main() {
     constexpr auto r1 = Result::Success;
     constexpr auto r2 = Result::Closed;
     tester.assertEqual(r1, r1, "Result values should be equal");
-    tester.expectException<TestException>([&] { tester.assertEqual(r1, r2, "Result values should be equal"); });
-    tester.assertNotEqual(r1, r2, "Result values should not be equal");
-    tester.expectException<TestException>([&] { tester.assertNotEqual(r1, r1, "Result values should not be equal"); });
-
+    // mismatched Result still uses assertEqual to trigger exception
+    tester.expectException<TestException>([&] {
+        tester.assertEqual(r1, r2, "Result values should be equal");
+    });
+    // since there's no assertNotEqual(Result,Result), test inequality via assertTrue
+    tester.assertTrue(r1 != r2, "Result values should not be equal");
+    tester.expectException<TestException>([&] {
+        tester.assertTrue(r1 == r2, "Result values should not be equal");
+    });
     // ---- Buffer8 tests ----
     using Common::Buffer8;
-    Buffer8 buf1 = {1, 2, 3};
-    Buffer8 buf2 = {1, 2, 3};
-    Buffer8 buf3 = {1, 2, 4};
+    const Buffer8 buf1 = {1, 2, 3}, buf2 = {1, 2, 3}, buf3 = {1, 2, 4};
     tester.assertEqual(buf1, buf2, "Buffer8 contents should be equal");
-    tester.expectException<TestException>([&] { tester.assertEqual(buf1, buf3, "Buffer8 contents should be equal"); });
-    tester.assertNotEqual(buf1, buf3, "Buffer8 contents should not be equal");
-    tester.expectException<TestException>([&] { tester.assertNotEqual(buf1, buf2, "Buffer8 contents should not be equal"); });
+    tester.expectException<TestException>([&] {
+        tester.assertEqual(buf1, buf3, "Buffer8 contents should be equal");
+    });
+    tester.assertNotEqual(buf1, buf2, "Buffer8 contents should not be equal (1)");
+    tester.expectException<TestException>([&] {
+        tester.assertNotEqual(buf1, buf3, "Buffer8 contents should not be equal (2)");
+    });
 
     // ---- Buffer64 tests ----
     using Common::Buffer64;
-    Buffer64 b641 = {1ULL, 2ULL, 3ULL};
-    Buffer64 b642 = {1ULL, 2ULL, 3ULL};
-    Buffer64 b643 = {1ULL, 2ULL, 4ULL};
+    const Buffer64 b641 = {1ULL, 2ULL, 3ULL}, b642 = {1ULL, 2ULL, 3ULL}, b643 = {1ULL, 2ULL, 4ULL};
     tester.assertEqual(b641, b642, "Buffer64 contents should be equal");
-    tester.expectException<TestException>([&] { tester.assertEqual(b641, b643, "Buffer64 contents should be equal"); });
-    tester.assertNotEqual(b641, b643, "Buffer64 contents should not be equal");
-    tester.expectException<TestException>([&] { tester.assertNotEqual(b641, b642, "Buffer64 contents should not be equal"); });
+    tester.expectException<TestException>(
+        [&] { tester.assertEqual(b641, b643, "Buffer64 contents should be equal"); }
+    );
+    tester.assertNotEqual(b641, b642, "Buffer64 contents should not be equal (1)");
+    tester.expectException<TestException>(
+        [&] { tester.assertNotEqual(b641, b643, "Buffer64 contents should not be equal (1)"); }
+    );
 
     // ---- Matrix tests ----
     using Gpu::Math::Matrix;
-    Matrix m1(2, 2);
-    m1.at(0, 0) = 1.0; m1.at(0, 1) = 2.0;
-    m1.at(1, 0) = 3.0; m1.at(1, 1) = 4.0;
-    Matrix m2(2, 2);
-    m2.at(0, 0) = 1.0; m2.at(0, 1) = 2.0;
-    m2.at(1, 0) = 3.0; m2.at(1, 1) = 4.0;
-    Matrix m3(2, 2);
-    m3.at(0, 0) = 1.0; m3.at(0, 1) = 2.0;
-    m3.at(1, 0) = 3.0; m3.at(1, 1) = 0.0;
-    tester.assertEqual(m1, m2, "Matrix contents should be equal");
-    tester.expectException<TestException>([&] { tester.assertEqual(m1, m3, "Matrix contents should be equal"); });
-    tester.assertNotEqual(m1, m3, "Matrix contents should not be equal");
-    tester.expectException<TestException>([&] { tester.assertNotEqual(m1, m2, "Matrix contents should not be equal"); });
+    Matrix m1(2, 2), m2(2, 2), m3(2, 2);
+    m1.at(0, 0) = 1;
+    m1.at(0, 1) = 2;
+    m1.at(1, 0) = 3;
+    m1.at(1, 1) = 4;
+
+    m2.at(0, 0) = 1;
+    m2.at(0, 1) = 2;
+    m2.at(1, 0) = 3;
+    m2.at(1, 1) = 4;
+
+    m3.at(0, 0) = 1;
+    m3.at(0, 1) = 2;
+    m3.at(1, 0) = 3;
+    m3.at(1, 1) = 5;
+
+    tester.assertEqual(m1, m2, "Matrix contents should be equal(1)");
+    tester.expectException<TestException>([&] {
+        tester.assertEqual(m1, m3, "Matrix contents should be equal(1)");
+    });
+    tester.assertNotEqual(m1, m3, "Matrix contents should not be equal(3)");
+    tester.expectException<TestException>([&] {
+        tester.assertNotEqual(m1, m2, "Matrix contents should not be equal(4)");
+    });
 
     return EXIT_SUCCESS;
 }
--- test/0003_tester_pointer_overloads.cpp
+++ test/0003_tester_pointer_overloads.cpp
@@ -1,5 +1,5 @@
-// file: test/0003\_tester\_pointer\_overloads.cpp
-// (c) 2025 Asymmetric Effort, LLC. [scaldwell@asymmetric-effort.com](mailto:scaldwell@asymmetric-effort.com)
+// file: test/0003_tester_pointer_overloads.cpp
+// (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
 
 #include "utils/test/Tester.h"
 #include "utils/test/TestException.h"
@@ -7,80 +7,109 @@
 #include <memory>
 #include <new>
 #include <cstdint>
+#include <cstdlib>
 
 int main() {
-Tester tester("0003_tester_pointer_overloads.cpp");
+    Tester tester("0003_tester_pointer_overloads.cpp");
 
-// ---- raw pointer tests ----
-int *raw_ptr = new int(42);
-tester.assertNotNull(raw_ptr, "raw pointer should not be null");
-tester.expectException<TestException>([&] { tester.assertNotNull(static_cast<int*>(nullptr), "raw pointer should not be null"); });
-delete raw_ptr;
+    // ---- raw void* pointer tests ----
+    void* vp = operator new(1);
+    tester.assertNotNull(vp, "raw void* should not be null");
+    tester.expectException<TestException>(
+        [&] { tester.assertNotNull(static_cast<void*>(nullptr), "raw void* should not be null"); }
+    );
+    operator delete(vp);
 
-// ---- unique_ptr<void> tests ----
-std::unique_ptr<void> upv(operator new(1));
-tester.assertNotNull(upv, "unique_ptr<void> should not be null");
-tester.expectException<TestException>([&] { tester.assertNotNull(std::unique_ptr<void>(), "unique_ptr<void> should not be null"); });
+    // ---- raw int* pointer tests ----
+    int* raw = new int(42);
+    tester.assertNotNull(raw, "raw int* should not be null");
+    tester.expectException<TestException>(
+        [&] { tester.assertNotNull(static_cast<int*>(nullptr), "raw int* should not be null"); }
+    );
+    delete raw;
 
-// ---- unique_ptr<int> tests ----
-auto upi = std::make_unique<int>(7);
-tester.assertNotNull(upi, "unique_ptr<int> should not be null");
-tester.expectException<TestException>([&] { tester.assertNotNull(std::unique_ptr<int>(), "unique_ptr<int> should not be null"); });
+    // ---- unique_ptr<int> tests ----
+    auto upi = std::make_unique<int>(7);
+    tester.assertNotNull(upi, "unique_ptr<int> should not be null");
+    tester.expectException<TestException>(
+        [&] { tester.assertNotNull(std::unique_ptr<int>(), "unique_ptr<int> should not be null"); }
+    );
 
-// ---- unique_ptr<unsigned> tests ----
-auto upu = std::make_unique<unsigned>(123u);
-tester.assertNotNull(upu, "unique_ptr<unsigned> should not be null");
-tester.expectException<TestException>([&] { tester.assertNotNull(std::unique_ptr<unsigned>(), "unique_ptr<unsigned> should not be null"); });
+    // ---- unique_ptr<unsigned> tests ----
+    auto upu = std::make_unique<unsigned>(123u);
+    tester.assertNotNull(upu, "unique_ptr<unsigned> should not be null");
+    tester.expectException<TestException>(
+        [&] { tester.assertNotNull(std::unique_ptr<unsigned>(), "unique_ptr<unsigned> should not be null"); }
+    );
 
-// ---- unique_ptr<float> tests ----
-auto upf = std::make_unique<float>(3.14f);
-tester.assertNotNull(upf, "unique_ptr<float> should not be null");
-tester.expectException<TestException>([&] { tester.assertNotNull(std::unique_ptr<float>(), "unique_ptr<float> should not be null"); });
+    // ---- unique_ptr<float> tests ----
+    auto upf = std::make_unique<float>(3.14f);
+    tester.assertNotNull(upf, "unique_ptr<float> should not be null");
+    tester.expectException<TestException>(
+        [&] { tester.assertNotNull(std::unique_ptr<float>(), "unique_ptr<float> should not be null"); }
+    );
 
-// ---- unique_ptr<double> tests ----
-auto upd = std::make_unique<double>(2.718);
-tester.assertNotNull(upd, "unique_ptr<double> should not be null");
-tester.expectException<TestException>([&] { tester.assertNotNull(std::unique_ptr<double>(), "unique_ptr<double> should not be null"); });
+    // ---- unique_ptr<double> tests ----
+    auto upd = std::make_unique<double>(2.718);
+    tester.assertNotNull(upd, "unique_ptr<double> should not be null");
+    tester.expectException<TestException>(
+        [&] { tester.assertNotNull(std::unique_ptr<double>(), "unique_ptr<double> should not be null"); }
+    );
 
-// ---- unique_ptr<char> tests ----
-auto upc = std::make_unique<char>('x');
-tester.assertNotNull(upc, "unique_ptr<char> should not be null");
-tester.expectException<TestException>([&] { tester.assertNotNull(std::unique_ptr<char>(), "unique_ptr<char> should not be null"); });
+    // ---- unique_ptr<char> tests ----
+    auto upc = std::make_unique<char>('x');
+    tester.assertNotNull(upc, "unique_ptr<char> should not be null");
+    tester.expectException<TestException>(
+        [&] { tester.assertNotNull(std::unique_ptr<char>(), "unique_ptr<char> should not be null"); }
+    );
 
-// ---- shared_ptr<void> / weak_ptr<void> tests ----
-std::shared_ptr<void> spv(operator new(1), [](void* p){ ::operator delete(p); });
-std::weak_ptr<void> wpv(spv);
-tester.assertNotNull(wpv, "weak_ptr<void> should not be null");
-tester.expectException<TestException>([&] { tester.assertNotNull(std::weak_ptr<void>() , "weak_ptr<void> should not be null"); });
+    // ---- shared_ptr<void> / weak_ptr<void> tests ----
+    std::shared_ptr<void> spv(operator new(1), [](void* p){ ::operator delete(p); });
+    std::weak_ptr<void> wpv(spv);
+    tester.assertNotNull(wpv, "weak_ptr<void> should not be null");
+    tester.expectException<TestException>(
+        [&] { tester.assertNotNull(std::weak_ptr<void>(), "weak_ptr<void> should not be null"); }
+    );
 
-// ---- shared_ptr<int> tests ----
-std::shared_ptr<int> spi = std::make_shared<int>(99);
-tester.assertNotNull(spi, "shared_ptr<int> should not be null");
-tester.expectException<TestException>([&] { tester.assertNotNull(std::shared_ptr<int>() , "shared_ptr<int> should not be null"); });
+    // ---- shared_ptr<int> tests ----
+    auto spi = std::make_shared<int>(99);
+    tester.assertNotNull(spi, "shared_ptr<int> should not be null");
+    tester.expectException<TestException>(
+        [&] { tester.assertNotNull(std::shared_ptr<int>(), "shared_ptr<int> should not be null"); }
+    );
 
-// ---- shared_ptr<unsigned> tests ----
-std::shared_ptr<unsigned> spu = std::make_shared<unsigned>(456u);
-tester.assertNotNull(spu, "shared_ptr<unsigned> should not be null");
-tester.expectException<TestException>([&] { tester.assertNotNull(std::shared_ptr<unsigned>() , "shared_ptr<unsigned> should not be null"); });
+    // ---- shared_ptr<unsigned> tests ----
+    auto spu = std::make_shared<unsigned>(456u);
+    tester.assertNotNull(spu, "shared_ptr<unsigned> should not be null");
+    tester.expectException<TestException>(
+        [&] { tester.assertNotNull(std::shared_ptr<unsigned>(), "shared_ptr<unsigned> should not be null"); }
+    );
 
-// ---- shared_ptr<float> tests ----
-std::shared_ptr<float> spf = std::make_shared<float>(1.23f);
-tester.assertNotNull(spf, "shared_ptr<float> should not be null");
-tester.expectException<TestException>([&] { tester.assertNotNull(std::shared_ptr<float>() , "shared_ptr<float> should not be null"); });
+    // ---- shared_ptr<float> tests ----
+    auto spf = std::make_shared<float>(1.23f);
+    tester.assertNotNull(spf, "shared_ptr<float> should not be null");
+    tester.expectException<TestException>(
+        [&] { tester.assertNotNull(std::shared_ptr<float>(), "shared_ptr<float> should not be null"); }
+    );
 
-// ---- shared_ptr<double> tests ----
-std::shared_ptr<double> spd = std::make_shared<double>(4.56);
-tester.assertNotNull(spd, "shared_ptr<double> should not be null");
-tester.expectException<TestException>([&] { tester.assertNotNull(std::shared_ptr<double>() , "shared_ptr<double> should not be null"); });
+    // ---- shared_ptr<double> tests ----
+    auto spd = std::make_shared<double>(4.56);
+    tester.assertNotNull(spd, "shared_ptr<double> should not be null");
+    tester.expectException<TestException>(
+        [&] { tester.assertNotNull(std::shared_ptr<double>(), "shared_ptr<double> should not be null"); }
+    );
 
-// ---- shared_ptr<char> tests ----
-std::shared_ptr<char> spc = std::make_shared<char>('z');
-tester.assertNotNull(spc, "shared_ptr<char> should not be null");
-tester.expectException<TestException>([&] { tester.assertNotNull(std::shared_ptr<char>() , "shared_ptr<char> should not be null"); });
+    // ---- shared_ptr<char> tests ----
+    auto spc = std::make_shared<char>('z');
+    tester.assertNotNull(spc, "shared_ptr<char> should not be null");
+    tester.expectException<TestException>(
+        [&] { tester.assertNotNull(std::shared_ptr<char>(), "shared_ptr<char> should not be null"); }
+    );
 
-// ---- unique_ptr<Gpu::Device::Interface> tests ----
-tester.expectException<TestException>([&] { tester.assertNotNull(std::unique_ptr<Gpu::Device::Interface>(), "unique_ptr<Interface> should not be null"); });
+    // ---- unique_ptr<Gpu::Device::Interface> tests ----
+    tester.expectException<TestException>(
+        [&] { tester.assertNotNull(std::unique_ptr<Gpu::Device::Interface>(), "unique_ptr<Interface> should not be null"); }
+    );
 
-return EXIT_SUCCESS;
-
+    return EXIT_SUCCESS;
 }
