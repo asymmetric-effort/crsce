2bd7018d9d5817b1cfe30517f5bdef985a188427
Sam Caldwell <github@samcaldwell.net>
2025-05-27 00:10:21 -0500

    add CRSCE/FileBuffer docstrings

--- include/CRSCE/FileBuffer.h
+++ include/CRSCE/FileBuffer.h
@@ -8,9 +8,15 @@
 #define CRSCE_FILE_BUFFER_H
 
 #include <vector>
-
+/**
+ * @property FILE_BUFFER_WIDTH
+ * @brief the size of the file buffer
+ */
 constexpr size_t FILE_BUFFER_WIDTH = 64;
-
+/**
+ * @property FileBuffer
+ * @brief an alias of std::vector<uint64_t> which should allow file reads in increments of 64-bytes.
+ */
 using FileBuffer = std::vector<uint64_t>;
 
 #endif //CRSCE_FILE_BUFFER_H
