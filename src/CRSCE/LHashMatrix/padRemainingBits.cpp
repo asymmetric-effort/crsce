39285cc4e4276e0ba38be8efa4760d436d09b91b
Sam Caldwell <github@samcaldwell.net>
2025-06-20 19:22:39 -0500

    extract LHashMatrix::padRemainingBits() from CRSCE::compress() to flatten the compress() function a bit.

--- src/CRSCE/LHashMatrix/padRemainingBits.cpp
+++ src/CRSCE/LHashMatrix/padRemainingBits.cpp
@@ -8,7 +8,9 @@
 void LHashMatrix::padRemainingBits(const std::size_t block_count, const std::size_t block_bits) {
     if (constexpr std::size_t total_bits = s * s; block_bits < total_bits) {
         std::cerr << "[CRSCE] Padding block " << block_count
-                  << " from " << block_bits << " to " << total_bits << " bits." << std::endl;
+                  << " from " << block_bits
+                  << " to " << total_bits
+                  << " bits." << std::endl;
         for (std::size_t i = block_bits; i < total_bits; ++i) {
             const CrossSumIndex r = static_cast<CrossSumIndex>(i / s);
             const CrossSumIndex c = static_cast<CrossSumIndex>(i % s);
