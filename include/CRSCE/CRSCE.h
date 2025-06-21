41138fcc32cdeca46cfc2db81ee3a6037887e53e
Sam Caldwell <github@samcaldwell.net>
2025-06-21 09:02:50 -0500

    make CRSCE::decompress() static and const for now to make sonarcloud and other tools quiet

--- include/CRSCE/CRSCE.h
+++ include/CRSCE/CRSCE.h
@@ -28,7 +28,7 @@ public:
 
     int compress();
 
-    int decompress();
+    static int decompress();
 
     // cppcheck-suppress unusedStructMember
     static constexpr size_t INPUT_BUFFER_SIZE = 1024 * 1024; // 1 MiB
