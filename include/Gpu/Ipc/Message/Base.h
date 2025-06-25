084f3efcf780575aabb6ca22386125853301b6ab
Sam Caldwell <github@samcaldwell.net>
2025-06-24 22:09:16 -0500

    Gpu::Ipc::Message::Base class

--- include/Gpu/Ipc/Message/Base.h
+++ include/Gpu/Ipc/Message/Base.h
@@ -1,39 +1,36 @@
 /**
- * @file include/Gpu/Ipc/Message.h
+ * @file include/Gpu/Ipc/Message/Base.h
  * @brief Define IPC Pipe Message Base Class
  * @copyright (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
  */
 
 #pragma once
 #include "Common/Buffer8.h"
-#include "Gpu/Ipc/Message/MessageType.h"
-#include "Common/AbstractPtr.h"
-#include <cstring>
 
-namespace Gpu::Ipc {
+namespace Gpu::Ipc::Message {
     /**
      * @class Message
      * @namespace Gpu::Ipc
      * @brief IPC Pipe Message Class
      * @ref docs/Gpu/Design/Gpu-Ipc-Message.md
      */
-    class Message {
+    class Base {
     public:
         /**
          * @name constructor
-         * @class Message
+         * @class Base
          * @namespace Gpu::Ipc
          * @brief initialize the Message object
          */
-        Message() =default;
+        Base() =default;
 
-        virtual ~Message() = default;
+        virtual ~Base() = default;
         /**
          * @name serialize
          * @brief virtual serialize function.  This is a last resort.
          * @return Common::Buffer8
          */
-        virtual Common::Buffer8 serialize() const { return (Common::Buffer8){}; };
+        [[nodiscard]] virtual Common::Buffer8 serialize() const { return (Common::Buffer8){}; };
         /**
          * @name deserialize
          * @brief virtual deserialize function
@@ -45,20 +42,14 @@ namespace Gpu::Ipc {
          * @brief virtual equality operator (compares serialized state).  True only if both are empty.
          * @return bool
          */
-        friend bool operator==(const Message& a, const Message& b) { return (a.serialize() == b.serialize()); };
+        friend bool operator==(const Base& a, const Base& b) { return (a.serialize() == b.serialize()); };
         /**
          * @name serialize
          * @brief virtual inequality operator (compares serialized state). True only if one is not empty.
          * @return bool
          */
-        friend bool operator!=(const Message& a, const Message& b) { return (a.serialize() != b.serialize()); };
+        friend bool operator!=(const Base& a, const Base& b) { return (a.serialize() != b.serialize()); };
     };
 
-    class InitializeGpuMessage final : public Message {
-    public:
-        Common::Buffer8 serialize() const override { throw std::runtime_error("undefined method"); };
-        void deserialize(const Common::Buffer8& buffer) override { throw std::runtime_error("undefined method"); };
-        friend bool operator==(const InitializeGpuMessage& a, const InitializeGpuMessage& b) {return false;}
-        friend bool operator!=(const InitializeGpuMessage& a, const InitializeGpuMessage& b) {return false;}
-    };
+
 } // namespace Gpu::Ipc
