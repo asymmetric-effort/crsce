dd0d2c8b153ee99fb0aac9e808e31c500e9aa236
Sam Caldwell <github@samcaldwell.net>
2025-06-29 18:26:02 -0500

    fix test_tester_assertEqual_sad.cpp

--- test/0000_tester_run_first/assertEqual/test_tester_assertEqual_sad.cpp
+++ test/0000_tester_run_first/assertEqual/test_tester_assertEqual_sad.cpp
@@ -12,43 +12,73 @@ int main() {
     Tester tester("test_tester_assertEqual_sad");
 
     tester.expectException<TestException>([&tester]() {
-        tester.assertEqual(42, 0, "int equality");
+        tester.assertEqual(
+            42,
+            0,
+            "int equality (expect failure)");
     });
 
     tester.expectException<TestException>([&tester]() {
-        tester.assertEqual(1u, 0u, "unsigned equality");
+        tester.assertEqual(
+            1u,
+            0u,
+            "unsigned equality (expect failure)");
     });
 
     tester.expectException<TestException>([&tester]() {
-        tester.assertEqual('a', 'b', "char equality");
+        tester.assertEqual(
+            'a',
+            'b',
+            "char equality (expect failure)");
     });
 
     tester.expectException<TestException>([&tester]() {
-        tester.assertEqual(3.14f, 3.01f, "float equality");
+        tester.assertEqual(
+            3.14f,
+            3.01f,
+            "float equality (expect failure)");
     });
 
     tester.expectException<TestException>([&tester]() {
-        tester.assertEqual(2.718, 2.008, "double equality");
+        tester.assertEqual(
+            2.718,
+            2.008,
+            "double equality (expect failure)");
     });
 
     tester.expectException<TestException>([&tester]() {
-        tester.assertEqual(1.00001f, 1.00010f, "float near-equal");
+        tester.assertEqual(
+            1.00001f,
+            1.00010f,
+            "float near-equal (expect failure)");
     });
 
     tester.expectException<TestException>([&tester]() {
-        tester.assertEqual(0.123456789, 0.123456888, "double near-equal");
+        tester.assertEqual(
+            0.123456789,
+            0.123456888,
+            "double near-equal (expect failure)");
     });
 
     tester.expectException<TestException>([&tester]() {
-        tester.assertEqual("foo_literal", "bar_literal", "string lhs should equal rhs");
+        tester.assertEqual(
+            "foo_literal",
+            "bar_literal",
+            "string lhs should equal rhs (expect failure)");
     });
 
     tester.expectException<TestException>([&tester]() {
-        tester.assertEqual(std::string("foo"), std::string("bar"), "string lhs should equal rhs");
+        tester.assertEqual(
+            std::string("foo"),
+            std::string("bar"),
+            "string lhs should equal rhs (expect failure)");
     });
 
     tester.expectException<TestException>([&tester]() {
-        tester.assertEqual('a', 'b', "string lhs should equal rhs");
+        tester.assertEqual(
+            'a',
+            'b',
+            "string lhs should equal rhs (expect failure)");
     });
 
     return tester.getFailCount() > 0 ? EXIT_FAILURE : EXIT_SUCCESS;
