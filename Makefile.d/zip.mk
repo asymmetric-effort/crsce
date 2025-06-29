3a7610ec017180f19be82e4787402a1c6d107671
Sam Caldwell <github@samcaldwell.net>
2025-06-29 13:28:00 -0500

    fixes for zip.mk

--- Makefile.d/zip.mk
+++ Makefile.d/zip.mk
@@ -1,7 +1,12 @@
-# clean.mk
+# zip.mk
 # (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
 
 .PHONY: zip
 zip: tree
 	@echo 'create build/project_files.zip'
-	zip -r ./build/project.zip . -x ".git/*" "build/*"
+	zip -r ./build/project.zip . \
+		-x ".git/*" \
+		-x "build/*" \
+		-x ".venv/*" \
+		-x ".idea/*" \
+		-x ".DS_Store"
