130906682785bd76888f57013c00a2597cf760e1
Sam Caldwell <github@samcaldwell.net>
2025-06-29 14:36:35 -0500

    move generate_temp_filename() to utils namespace

--- src/utils/generate_temp_filename.cpp
+++ src/utils/generate_temp_filename.cpp
@@ -9,19 +9,21 @@
 #include <random>
 #include <sstream>
 
-/**
- * @name generate_temp_filename
- * @brief generate a temporary filename with a given prefix and extension.
- * @param prefix
- * @param ext
- * @return std::string
- */
-std::string generate_temp_filename(const std::string &prefix, const std::string &ext) {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    static std::uniform_int_distribution dist(10000, 99999);
+namespace utils {
+    /**
+     * @name generate_temp_filename
+     * @brief generate a temporary filename with a given prefix and extension.
+     * @param prefix
+     * @param ext
+     * @return std::string
+     */
+    std::string generate_temp_filename(const std::string &prefix, const std::string &ext) {
+        static std::random_device rd;
+        static std::mt19937 gen(rd());
+        static std::uniform_int_distribution dist(10000, 99999);
 
-    std::ostringstream oss;
-    oss << get_tmp_dir() << "/" << prefix << "_" << dist(gen) << "." << ext;
-    return oss.str();
+        std::ostringstream oss;
+        oss << get_tmp_dir() << "/" << prefix << "_" << dist(gen) << "." << ext;
+        return oss.str();
+    }
 }
