d3e83c6ef9c57099aba70f5bd72c3e8a4f19136e
Sam Caldwell <github@samcaldwell.net>
2025-07-11 01:17:29 -0500

    capture all output when calling utils::exec()

--- include/utils/exec.h
+++ include/utils/exec.h
@@ -18,10 +18,12 @@ namespace utils {
     static std::string exec(const std::string& cmd) {
         std::array<char, 256> buffer{};
         std::string result;
-        const std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), &pclose);
+        // redirect stderr (2) into stdout (1) so nothing leaks to the real stderr
+        const std::string full_cmd = cmd + " 2>&1";
+        const std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(full_cmd.c_str(), "r"), &pclose);
         if (!pipe) throw std::runtime_error("popen() failed!");
         while (!feof(pipe.get())) {
-            if (size_t n = fread(buffer.data(), 1, buffer.size(), pipe.get()); n > 0)
+            if (const size_t n = fread(buffer.data(), 1, buffer.size(), pipe.get()); n > 0)
                 result.append(buffer.data(), n);
         }
         return result;
