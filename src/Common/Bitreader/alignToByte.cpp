fb5ebe0510a0e6001b81f1dfc6f534ba0d0dee03
Sam Caldwell <github@samcaldwell.net>
2025-07-13 10:12:36 -0500

    make the linter happy

--- src/Common/Bitreader/alignToByte.cpp
+++
@@ -1,8 +0,0 @@
-// file: src/Common/BitReader/alignToByte.cpp
-// (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
-
-#include "Common/BitReader.h"
-
-void BitReader::alignToByte() {
-    bitsRemaining = 0;
-}
--- src/Common/Bitreader/constructor.cpp
+++
@@ -1,7 +0,0 @@
-// file: src/Common/BitReader/constructor.cpp
-// (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
-
-#include "Common/BitReader.h"
-
-BitReader::BitReader(std::istream& in)
-    : stream(in), buffer(0), bitsRemaining(0) {}
