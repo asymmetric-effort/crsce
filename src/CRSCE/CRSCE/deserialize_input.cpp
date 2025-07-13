5188fd747f2bf2c611edbf785ed1f094bd53eb11
Sam Caldwell <github@samcaldwell.net>
2025-07-12 21:16:08 -0500

    implementing deserialize()

--- src/CRSCE/CRSCE/deserialize_input.cpp
+++ src/CRSCE/CRSCE/deserialize_input.cpp
@@ -11,33 +11,17 @@
 #include "CRSCE/CrossSum/DiagonalSumMatrix/DiagonalSumMatrix.h"
 #include "CRSCE/CrossSum/AntidiagonalSumMatrix/AntidiagonalSumMatrix.h"
 
-void CRSCE::deserialize_input(const LateralSumMatrix& LSM, const VerticalSumMatrix& VSM,
-    const DiagonalSumMatrix& XSM, const AntidiagonalSumMatrix& DSM, const LHashMatrix& LHASH) {
-
-    // 1) Verify and consume block header and size
-    std::string hdr(HEADER.size(), '\0');
-    inputStream.read(&hdr[0], static_cast<std::streamsize>(HEADER.size()));
-    if (hdr != HEADER) {
-        throw std::runtime_error("[CRSCE] Invalid block header");
-    }
-
-    // 2) Read block size s (must match configured value)
-    uint16_t s = 0;
-    inputStream.read(reinterpret_cast<char*>(&s), sizeof(s));
-    if (s != this->blockSize) {
-        throw std::runtime_error("[CRSCE] Unexpected block size");
-    }
+void CRSCE::deserialize_input(const LateralSumMatrix& LSM,
+                              const VerticalSumMatrix& VSM,
+                              const DiagonalSumMatrix& XSM,
+                              const AntidiagonalSumMatrix& DSM,
+                              const LHashMatrix& LHASH) {
 
     // 3) Read cross-sum bit-width b
     const uint8_t b = static_cast<uint8_t>(std::ceil(std::log2(s)));
 
     // 4) Read row-hash matrix (LHASH)
-    lhMatrix.clear();
-    for (size_t r = 0; r < s; ++r) {
-        std::string hash(SHA256::DIGEST_SIZE, '\0');
-        inputStream.read(&hash[0], static_cast<std::streamsize>(SHA256::DIGEST_SIZE));
-        lhMatrix.push_back(hash);
-    }
+    LHASH.deserialize(inputStream);
 
     // 5) Prepare BitReader for unpacking cross-sums
     BitReader br(inputStream);
---
+++ src/CRSCE/LHashMatrix/deserialize.cpp
@@ -0,0 +1,25 @@
+/**
+* @file src/CRSCE/LHashMatrix/deserialize.cpp
+ * @copyright (c) 2025 Asymmetric Effort, LLC. <scaldwell@asymmetric-effort.com>
+ */
+
+#include "CRSCE/LHashMatrix.h"
+#include "utils/Exceptions/InvalidHashLength.h"
+
+/**
+ * @name deserialize
+ * @class LHashMatrix
+ * @brief This method deserializes the LHASH content from the input stream
+ * @param is std::istream
+ * @throws Exceptions::InvalidHashLength()
+ */
+void LHashMatrix::deserialize(std::istream& is) const {
+    for (CrossSumIndex r = 0; r < s; ++r) {
+        std::string hash(SHA256::DIGEST_SIZE, '\0');
+        is.read(&hash[0], SHA256::DIGEST_SIZE);
+        if (!is) {
+            throw Exceptions::InvalidHashLength(0);
+        }
+        row_hash_data[r]=hash;
+    }
+}
